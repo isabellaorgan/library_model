56f99e2d61dfd309c93388206a1ca18a9e2d9364
Isabella Organ <isabellaorgan@gmail.com>
2015-09-17 10:37:13 -0700

    reorder code to make additons to and removal from shelves or library make more sense

--- main.js
+++ main.js
@@ -4,22 +4,22 @@ var library = require('./library.js');
 var shelves = require('./shelves.js');
 var books = require('./books.js');
 
-
-books.bookModule.makeBook("Gillian Flynn", "Gone Girl", "Mystery");
-books.bookModule.makeBook("Mark Z. Danielewski", "House of Leaves", "Ergodic");
-
 /** @namespace shelves.shelfModule */
 books.bookModule.makeBook("Stephen King", "It", "Horror");
 shelves.shelfModule.buildShelf("Horror");
+shelves.shelfModule.addBook("Stephen King", "It", "Horror");
+shelves.shelfModule.removeBook("Stephen King", "It", "Horror");
 shelves.shelfModule.deleteShelf("Horror");
+books.bookModule.makeBook("Gillian Flynn", "Gone Girl", "Mystery");
 shelves.shelfModule.buildShelf("Mystery");
-shelves.shelfModule.deleteShelf("Mystery");
-shelves.shelfModule.buildShelf("Ergodic");
-shelves.shelfModule.deleteShelf("Ergodic");
-shelves.shelfModule.addBook("Stephen King", "It", "Horror");shelves.shelfModule.removeBook("Stephen King", "It", "Horror");
 shelves.shelfModule.addBook("Gillian Flynn", "Gone Girl", "Mystery");
 shelves.shelfModule.removeBook("Gillian Flynn", "Gone Girl", "Mystery");
+shelves.shelfModule.deleteShelf("Mystery");
+books.bookModule.makeBook("Mark Z. Danielewski", "House of Leaves", "Ergodic");
+shelves.shelfModule.buildShelf("Ergodic");
 shelves.shelfModule.addBook("Mark Z. Danielewski", "House of Leaves", "Ergodic");
 shelves.shelfModule.removeBook("Mark Z. Danielewski", "House of Leaves", "Ergodic");
+shelves.shelfModule.deleteShelf("Ergodic");
+
 
 
